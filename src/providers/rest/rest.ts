dbefb02cb47defa65298aced6cf888d95fcbd675
Taylor Jackson <tjackson8@wisc.edu>
2018-11-06 11:34:09 -0600

    more reasonable color

--- src/providers/rest/rest.ts
+++ src/providers/rest/rest.ts
@@ -9,7 +9,7 @@ import { Injectable } from '@angular/core';
 */
 @Injectable()
 export class RestProvider {
-  apiUrl = 'http://192.168.1.9:8000';
+  apiUrl = 'http://recyclopedia.2yqbpwp3fb.us-west-2.elasticbeanstalk.com';
   constructor(public http: HttpClient) {
     console.log('Hello RestProvider Provider');
   }
