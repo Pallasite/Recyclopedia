bc26a524127752ee823584d8a8622c278c6eac00
Jack Cerhan <jackcerhan@Jacks-MacBook-Pro-4.local>
2018-11-12 00:46:31 -0600

    tried to remove pycache stuff from commit history

--- recycle_backend/recycle_db/views.py
+++ recycle_backend/recycle_db/views.py
@@ -1,8 +1,12 @@
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from recycle_db.models import Recyclable
 from recycle_db.serializers import RecyclableSerializer
+
+class
+
 
 @api_view(['GET', 'POST'])
 def recyclable_list(request):
@@ -36,24 +40,8 @@ def recyclable_search(request, search):
     serializer = RecyclableSerializer(filtered_search, many=True)
     return Response(serializer.data)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def recyclable_detail(request, item):
-    try:
-        recyclable = Recyclable.objects.get(item=item)
-    except Recyclable.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
-        serializer = RecyclableSerializer(recyclable)
-        return Response(serializer.data)
+class recyclable_detail(generics.RetrieveUpdateDestroyAPIView)
+    queryset = Recyclable.objects.all()
+    serializer_class = RecyclableSerializer
 
-    elif request.method == 'PUT':
-        serializer = RecyclableSerializer(recyclable, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        recyclable.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
