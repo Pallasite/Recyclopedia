624e3296a993fdf9260469306db0710eb363ce0e
wchen <wchen@usgs.gov>
2018-11-15 23:10:25 -0600

    A bit of work on the login authenticator- not functioning yet

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HTTP } from '@ionic-native/http';
+import { RestProvider } from '../../providers/rest/rest'
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
+
 export class FormsPage {
+
+  constructor(public loginHTTP : HTTP){}
+
   login = {}
   logForm() {
+
     console.log(this.login)
     var username = this.login[0];
     var pw = this.login[1];
-
-    
+    var params = {};
+    var token = "Authentication: a";
+    var headers = {"token":token};
+    this.loginHTTP.get(RestProvider.apiUrl, {}, headers);
   }
 }
