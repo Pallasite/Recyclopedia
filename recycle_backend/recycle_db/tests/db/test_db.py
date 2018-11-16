0f30884738865ca4f849ff3940f2149f00290733
wchen <wchen@usgs.gov>
2018-11-16 15:27:51 -0600

    Preliminary skeleton

---
+++ recycle_backend/recycle_db/tests/db/test_db.py
@@ -0,0 +1,39 @@
+import unittest
+#import psycopg2
+
+# Runs the whole test suite for unit tests
+# pertaining to the database
+# TODO replace each print(Test not implemented) with calls to test methods
+   
+
+test_count = 0
+test_pass = 0 
+
+print("DATABASE Tests\n-----------------")
+
+print("Test 1: CONNECTION")
+print("**Test a connection to a local database**\n\tResult:\t")
+print("\tTest not implemented\n")
+test_count = test_count + 1
+
+print("Test 2: SIMPLE QUERY")
+print("**Test a connection to a local database**\n\tResult:\t")
+print("\tTest not implemented\n")
+test_count = test_count + 1
+
+print("Test 3: SIMPLE QUERY")
+print("**Test a connection to a local database**\n\tResult:\t")
+print("\tTest not implemented\n")
+test_count = test_count + 1
+
+print("Test 4: FIND GROUPS")
+print("**Test a query to finish.**\n\tResult:\t")
+print("\tTest not implemented\n")
+test_count = test_count + 1
+
+print("Test 5: ")
+print("**Test a connection to a local database**\n\tResult:\t")
+print("\tTest not implemented\n")
+test_count = test_count + 1
+
+print("Total Results: " + str(test_pass) + " out of " + str(test_count) + " passed.")
