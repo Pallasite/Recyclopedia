bf505d52b840d4b7762ef726ab8199636ef82f93
Jack Cerhan <jcerhan@wisc.edu>
2018-11-30 14:33:57 -0600

    fixed login token problems

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -30,7 +30,6 @@ export class FormsPage {
       "username": String
     }
   }
-  
   login = {
     username: '',
     password: ''
@@ -44,11 +43,11 @@ export class FormsPage {
     if (this.login) {
       this.restProvider.userLogin(this.login)
         .then(data => {
-          // var response = data; 
+          var response = data; 
           console.log("data: " , data);
-          this.global.token = data.token;
+          this.global.token = response['token'];
            console.log("Token updated: " , this.global.token);
-           var user = data.user;
+           var user = response['user'];
 
            console.log("user: " , user);
         });
