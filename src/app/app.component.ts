60b3a6bab59a52b01fc2a145ee68cf968ba66713
Taylor Jackson <tjackson8@wisc.edu>
2018-11-30 14:34:22 -0600

    Sidebar works now, will direct user to page chosen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,30 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
 import { TabsPage } from '../pages/tabs/tabs';
+import { SearchPage } from '../pages/search/search';
+import { LocationsPage } from '../pages/locations/locations';
+import { HomePage } from '../pages/home/home';
+import { UserProfile } from '../pages/profile/profile';
+import { Favorites } from '../pages/favorites/favorites';
+
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage: any = TabsPage;
+  rootPage: any = HomePage;
+  // menu.enable(true);
+  @ViewChild(Nav) nav;
+  SearchPage: any = SearchPage;
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar, 
+    splashScreen: SplashScreen,
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    ) {
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here we can do any higher level native things we might need
@@ -19,4 +32,30 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+  
+  // NavCtrl can't be injected as this app file is the parent
+  // to all components. So we must use ViewChild to use nav.
+
+  
+  openPage(page) {
+    if (page == 'SearchPage') {
+      this.nav.push(SearchPage);
+    }
+    else if (page == 'HomePage') {
+      this.nav.push(HomePage);
+    }
+    else if (page == 'LocationsPage') {
+      this.nav.push(LocationsPage);
+    }
+    else if (page == 'UserProfile') {
+      this.nav.push(UserProfile);
+    }
+    else if (page == 'Favorites') {
+      this.nav.push(Favorites);
+    }
+  }
+
+
+
+
 }
--- src/pages/global/global.service.ts
+++ src/pages/global/global.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class GlobalService {
     // for when we want to make it inaccessible to submit from an unauthorized account
-//   public adminState: boolean = false;
+    // public adminState: boolean = false;
     public token: String;
 }
